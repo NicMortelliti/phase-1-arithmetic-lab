2ad8bc1341b3fc99ef51abbf20ddfacbcb8e8fe4
Nicolas Mortelliti <npmortelliti@gmail.com>
2022-02-01 10:48:08 -0800

    commit tree 7d6f722bfec40aa7108cc3d946c00aea7db7c8af

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Multiply
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Random
+const random = Math.floor(Math.random() * 10) + 1;
+
+// Mod
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+// Max
+const max = Math.max(20, 3, 17);
